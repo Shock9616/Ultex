880749f079610d041a3a088ebf0ca9418f65e063
Kaleb Rosborough <kaleb@rosborough.ca>
2022-03-23 15:31:17 -0700

    Beginning to work on music playback

--- ultex/extensions/music.py
+++ ultex/extensions/music.py
@@ -16,9 +16,6 @@ TOKEN = os.environ["TOKEN"]
 LAVALINK_PASSWORD = os.environ["LAVALINK_PASSWORD"]
 LAVASNEK_PREFIX = os.environ["LAVASNEK_PREFIX"]
 
-# If True connect to voice with the hikari gateway instead of lavasnek_rs's
-HIKARI_VOICE = False
-
 
 class EventHandler:
     """Events from the Lavalink server"""
@@ -46,6 +43,9 @@ class EventHandler:
 plugin = lightbulb.Plugin("Music")
 
 
+# ---------- Private Functions ----------
+
+
 async def _join(ctx: lightbulb.Context) -> Optional[hikari.Snowflake]:
     assert ctx.guild_id is not None
 
@@ -58,20 +58,13 @@ async def _join(ctx: lightbulb.Context) -> Optional[hikari.Snowflake]:
 
     channel_id = voice_state[0].channel_id
 
-    if HIKARI_VOICE:
-        assert ctx.guild_id is not None
-
-        await plugin.bot.update_voice_state(ctx.guild_id, channel_id, self_deaf=True)
-        connection_info = await plugin.bot.d.lavalink.wait_for_full_connection_info_insert(ctx.guild_id)
-
-    else:
-        try:
-            connection_info = await plugin.bot.d.lavalink.join(ctx.guild_id, channel_id)
-        except TimeoutError:
-            await ctx.respond(
-                "I was unable to connect to the voice channel, maybe missing permissions? or some internal issue."
-            )
-            return None
+    try:
+        connection_info = await plugin.bot.d.lavalink.join(ctx.guild_id, channel_id)
+    except TimeoutError:
+        await ctx.respond(
+            "I was unable to connect to the voice channel, maybe missing permissions? or some internal issue."
+        )
+        return None
 
     await plugin.bot.d.lavalink.create_session(connection_info)
 
@@ -89,12 +82,12 @@ async def start_lavalink(event: hikari.ShardReadyEvent) -> None:
         .set_host("127.0.0.1").set_password(LAVALINK_PASSWORD)
     )
 
-    if HIKARI_VOICE:
-        builder.set_start_gateway(False)
-
     lava_client = await builder.build(EventHandler())
 
     plugin.bot.d.lavalink = lava_client
+
+
+# ---------- Command Functions ----------
 
 
 @plugin.command()
@@ -117,13 +110,7 @@ async def leave(ctx: lightbulb.Context) -> None:
     """Leaves the voice channel the bot is in, clearing the queue."""
 
     await plugin.bot.d.lavalink.destroy(ctx.guild_id)
-
-    if HIKARI_VOICE:
-        if ctx.guild_id is not None:
-            await plugin.bot.update_voice_state(ctx.guild_id, None)
-            await plugin.bot.d.lavalink.wait_for_connection_info_remove(ctx.guild_id)
-    else:
-        await plugin.bot.d.lavalink.leave(ctx.guild_id)
+    await plugin.bot.d.lavalink.leave(ctx.guild_id)
 
     # Destroy nor leave remove the node nor the queue loop, you should do this manually.
     await plugin.bot.d.lavalink.remove_guild_node(ctx.guild_id)
@@ -194,7 +181,8 @@ async def skip(ctx: lightbulb.Context) -> None:
     if not skip:
         await ctx.respond("Nothing to skip")
     else:
-        # If the queue is empty, the next track won't start playing (because there isn't any),
+        # If the queue is empty, the next track won't
+        # start playing (because there isn't any),
         # so we stop the player.
         if not node.queue and not node.now_playing:
             await plugin.bot.d.lavalink.stop(ctx.guild_id)
@@ -226,7 +214,10 @@ async def resume(ctx: lightbulb.Context) -> None:
 
 @plugin.command()
 @lightbulb.add_checks(lightbulb.guild_only)
-@lightbulb.command("nowplaying", "Gets the song that's currently playing.", aliases=["np"])
+@lightbulb.command("nowplaying",
+    "Gets the song that's currently playing.",
+    aliases=["np"]
+)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def now_playing(ctx: lightbulb.Context) -> None:
     """Gets the song that's currently playing."""
@@ -245,15 +236,21 @@ async def now_playing(ctx: lightbulb.Context) -> None:
 @lightbulb.add_checks(lightbulb.guild_only)
 @lightbulb.add_checks(lightbulb.owner_only)  # Optional
 @lightbulb.option(
-    "args", "The arguments to write to the node data.", required=False, modifier=lightbulb.OptionModifier.CONSUME_REST
+    "args",
+    "The arguments to write to the node data.",
+    required=False,
+    modifier=lightbulb.OptionModifier.CONSUME_REST
 )
 @lightbulb.command("data", "Load or read data from the node.")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def data(ctx: lightbulb.Context) -> None:
-    """Load or read data from the node.
+    """
+    Load or read data from the node.
 
-    If just `data` is ran, it will show the current data, but if `data <key> <value>` is ran, it
-    will insert that data to the node and display it."""
+    If just `data` is ran, it will show the current data, but if
+    `data <key> <value>` is ran, it will insert that data to the
+    node and display it.
+    """
 
     node = await plugin.bot.d.lavalink.get_guild_node(ctx.guild_id)
 
@@ -271,25 +268,15 @@ async def data(ctx: lightbulb.Context) -> None:
     await ctx.respond(node.get_data())
 
 
-if HIKARI_VOICE:
-
-    @plugin.listener(hikari.VoiceStateUpdateEvent)
-    async def voice_state_update(event: hikari.VoiceStateUpdateEvent) -> None:
-        plugin.bot.d.lavalink.raw_handle_event_voice_state_update(
-            event.state.guild_id,
-            event.state.user_id,
-            event.state.session_id,
-            event.state.channel_id,
-        )
-
-    @plugin.listener(hikari.VoiceServerUpdateEvent)
-    async def voice_server_update(event: hikari.VoiceServerUpdateEvent) -> None:
-        await plugin.bot.d.lavalink.raw_handle_event_voice_server_update(event.guild_id, event.endpoint, event.token)
+# --------- Plugin Load and Unload Functions ----------
 
 
 def load(bot: lightbulb.BotApp) -> None:
+    """ Load commands and plugins to the bot """
     bot.add_plugin(plugin)
 
 
 def unload(bot: lightbulb.BotApp) -> None:
+    """ Unload commands and plugins from the bot """
     bot.remove_plugin(plugin)
+
