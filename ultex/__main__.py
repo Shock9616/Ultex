6a9a1b15134b5fd3761d2e66efded444777d2818
Kaleb Rosborough <kaleb@rosborough.ca>
2022-03-30 15:42:49 -0700

    Added event checks to __main__.py

--- ultex/__main__.py
+++ ultex/__main__.py
@@ -24,13 +24,15 @@ if __name__ == "__main__":
     @bot.listen()
     async def on_starting(event: hikari.StartingEvent) -> None:
         """ Listener function for when the bot starts up """
-        bot.d.aio_session = aiohttp.ClientSession()
+        if event:
+            bot.d.aio_session = aiohttp.ClientSession()
 
 
     @bot.listen()
     async def on_stopping(event: hikari.StoppingEvent) -> None:
         """ Listerner function for when the bot shuts down """
-        await bot.d.aio_session.close()
+        if event:
+            await bot.d.aio_session.close()
 
     # Use uvloop instead of asyncio if not being run on windows
     if os.name != "win32":
